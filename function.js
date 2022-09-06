9f32c4f040184df85b42ff6c9dd838884be16eac
Divyansh Dixir <divyanshdixit96@gmail.com>
2022-09-06 22:00:58 +0530

    js functions, datatypes

---
+++ function.js
@@ -0,0 +1,9 @@
+function sum(x=0,y=1){
+
+    value = 10;
+
+    let res = x + y;
+    return res;   
+}
+console.log(sum(10)); //11
+console.log(value)
---
+++ index.js
@@ -0,0 +1,13 @@
+{
+    const t = 10; // number, string, undefined, boolean
+    t=20;
+    console.log(t)
+}
+
+{
+    let t = 10;
+    t=20;
+    console.log(t)// 20
+}
+
+console.log(t)  // 20 
